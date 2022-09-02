dda6e7a30630b8d43ca1070d0077ae9232619b88
Francocdev <frankko212@gmail.com>
2022-09-02 17:33:42 -0300

    Allmost all the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+import Footer from "./components/Footer"
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Meme />
+            <Footer />
+        </div>
+    )
+}
+
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <footer className="footer">
+            <span className="footer--text">Made with ❤ by <a href="https://francoc.ar">Franco</a></span>
+        </footer>
+    )
+}
